a31c71695d35818357fad893d767db5a5b73a070
liuguiyangnwpu <liuguiyangnwpu@163.com>
2017-09-14 12:37:39 +0800

    修改在nvidia-docker中的对应的配置文件

--- scripts/darknet.run.sh
+++ scripts/darknet.run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#!/bin/bash
-docker run -it \
+nvidia-docker run -it \
     -v /home/ai-i-liuguiyang/repos_ssd/:/root/repos \
     -p 9997:9997 \
-    ubuntu:darknet /bin/bash
+    nvidia/cuda:grpc /bin/bash
